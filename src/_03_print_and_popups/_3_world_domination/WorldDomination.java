70721ad272fa1c373331daddb9b6213fef0bf337
ColbySchexnayder <schexnayderhusser@gmail.com>
2023-11-02 08:51:48 -0700

    Finished Prints and Popups

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,20 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog("Do you know how to code?");
+		input = input.toLowerCase();
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (input.contains("y")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up for classes");
+		}
 	}
 }
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,35 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+
+		// Create a variable to hold the user's score. Set it equal to zero.
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
+		String answer = JOptionPane.showInputDialog("What is 2+2?");
+		// 2. Ask the user a question
+		if (answer.equals("4")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong! The answer is 4");
+			score--;
+		}
 		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
+		answer = JOptionPane.showInputDialog("");
 		
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
-		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Your score is: " + score);
+		// 3. Use an if statement to check if their answer is correct
+
+		// 4. if the user's answer was correct, add one to their score
+
+		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
+		// Option: Subtract a point from their score for a wrong answer
+
+		// After all the questions have been asked, tell the user their final score
+
 	}
 }
